b086824650336e3c6bd9f7c0b53339868100905e
Kenichi Saito <hajime@studiohff.net>
2017-12-09 15:35:55 +0900

    アイコンダウンロード機能を実装

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import * as Commander from "commander";
 import * as Nunjucks from "nunjucks";
 import * as TwitterText from "twitter-text";
 
+import { downloadMedia, downloadProfileImage } from "./downloadImage";
 import * as FileSystemUtil from "./fileSystemUtil";
 import TweetRepository from "./tweetRepository";
 import TwitterGateway from "./twitterGateway";
@@ -50,7 +51,35 @@ function setUpCommandLineParser(tweetRepository: TweetRepository): any {
             (async () => {
                 const tweet = await twitterGateway.getTweet((tweetId as string));
 
-                await tweetRepository.insert(tweet);
+                const iconDirName = "./db/icons/";
+                if (fs.existsSync(iconDirName) === false) {
+                    fs.mkdirSync(iconDirName);
+                }
+
+                const iconBaseFileName = tweet.user.screen_name;
+
+                let iconFileName: string | null = null;
+                for (const iconExtension of ["jpg", "png"]) {
+                    const iconFullPath = path.join(iconDirName, `${iconBaseFileName}.${iconExtension}`);
+
+                    if (fs.existsSync(iconFullPath)) {
+                        iconFileName = `${iconBaseFileName}.${iconExtension}`;
+                    }
+                }
+
+                if (iconFileName === null) {
+                    iconFileName = await downloadProfileImage(tweet.user.profile_image_url,
+                        iconDirName, iconBaseFileName);
+                }
+
+                /* tslint:disable:object-literal-sort-keys */
+                const newDoc = {
+                    originalTweet : tweet,
+                    iconFileName,
+                };
+                /* tslint:enable:object-literal-sort-keys */
+
+                await tweetRepository.insert(newDoc);
             })()
             .catch((error) => {
                 console.log(error);
@@ -91,7 +120,7 @@ function setUpCommandLineParser(tweetRepository: TweetRepository): any {
                 const dirName = FileSystemUtil.createDirName();
                 fs.mkdirSync(dirName);
 
-                for (const staticFileName of ["icon.jpg", "normalize.css", "styles.css"]) {
+                for (const staticFileName of ["normalize.css", "styles.css"]) {
                     const src = path.join("./templates", staticFileName);
                     const dest = path.join(dirName, staticFileName);
 
@@ -99,13 +128,19 @@ function setUpCommandLineParser(tweetRepository: TweetRepository): any {
                 }
 
                 for (const tweet of tweets) {
-                    tweet.autoLinkedText = TwitterText.autoLink(tweet.text, { urlEntities: tweet.entities.urls });
+                    tweet.autoLinkedText = TwitterText.autoLink(
+                        tweet.originalTweet.text,
+                        { urlEntities: tweet.originalTweet.entities.urls }
+                    );
+
+                    const src = path.join("./db/icons/", tweet.iconFileName);
+                    const dest = path.join(dirName,  tweet.iconFileName);
+                    fs.copyFileSync(src, dest);
                 }
 
                 /* tslint:disable:object-literal-sort-keys */
                 const data = {
                     tweets,
-                    iconFileName: "icon.jpg",   // TODO: implement!
                     images: [],                 // TODO: implement!
                 };
                 /* tslint:enable:object-literal-sort-keys */
